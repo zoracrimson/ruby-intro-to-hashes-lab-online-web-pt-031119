005e22f82b0ecacfacfd1abc74f0f8ca7960a225
Kat Maldon <katharinamaldon@gmail.com>
2019-04-04 17:48:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,12 @@ def monopoly_with_third_tier
 	  railroads: {
 	    pieces: 4, 
 	    names: {},
-	    rent_in_dollars: {one_piece_owned: 25, }
+	    rent_in_dollars: {
+	      one_piece_owned: 25,
+	      two_pieces_owned: 50,
+	      three_pieces_owned: 100,
+	      four_pieces_owned: 200
+	    }
 	  }
 	}
 end
