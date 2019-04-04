9fef1d4a51901301123b023b94ebbaf2534f4543
Kat Maldon <katharinamaldon@gmail.com>
2019-04-04 18:00:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -57,7 +57,8 @@ def monopoly_with_fourth_tier
 	        "mortgage_value" => "400"
 	      },
 	      shortline: {
-	        "mortgage_value" = "$800"
+	        "mortgage_value" => "$800"
+	      }
 	      },
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
