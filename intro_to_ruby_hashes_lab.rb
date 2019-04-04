718a2254857931fddd9e76fae2a234259f605ab9
Kat Maldon <katharinamaldon@gmail.com>
2019-04-04 17:49:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,6 +29,7 @@ def monopoly_with_third_tier
 	    names: {
 	      reading_railroad: {},
 	      pennsylvania_railroad: {},
+	      b_and_o_railroad: {},
 	      
 	    },
 	    rent_in_dollars: {
