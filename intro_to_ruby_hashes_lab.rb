6c5114bdff2f6e2c2ae7e58d41181188506b15bf
Kat Maldon <katharinamaldon@gmail.com>
2019-04-04 17:54:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -50,7 +50,8 @@ def monopoly_with_fourth_tier
 	      reading_railroad: {"mortgage_value" => "$100" },
 	      pennsylvania_railroad: {"mortgage_value" => "$200"},
 	      b_and_o_railroad: {"mortgage_value" => "400"},
-	      shortline: {"mortgage_value" = "$800"},
+	      shortline: {"mortgage_value" = "$800"}
+	      },
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
 	      two_pieces_owned: 50,
